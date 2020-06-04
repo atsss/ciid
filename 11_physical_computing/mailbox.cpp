c337c215c369bfecdd24540170953d2466383bd2
atsss <atsushi0623itoh@gmail.com>
2020-06-04 09:35:05 -0600

    add radio motor

--- 11_physical_computing/mailbox.cpp
+++ 11_physical_computing/mailbox.cpp
@@ -2,11 +2,12 @@
 #include <Servo.h>
 
 LiquidCrystal lcd(12 ,11 ,5 ,4 ,3 , 2);
-Servo myServo;
+Servo flagServo;
 
 const int switchPin = 13;
-const int servoPin = 9;
-const int angle = 90;
+const int flagPin = 9;
+const int motorPin = 8;
+const int flagAngle = 90;
 const int messageNum = 3;
 const int lineNum = 2;
 String messages[messageNum][lineNum] = {
@@ -18,14 +19,16 @@ int index = 0;
 int prevIndex = 0;
 const int interval = 5000;
 unsigned long prevTime = 0;
-enum State { NORMAL, WAITING, SHOWN, READ };
+enum State { NORMAL, WAITING, SHOWN, READ, ARCAHIVED };
 State messageState = NORMAL;
 
 void setup() {
   Serial.begin(9600);
   lcd.begin(16, 2);
-  myServo.attach(servoPin);
+  flagServo.attach(flagPin);
   pinMode(switchPin, INPUT);
+  pinMode(motorPin, OUTPUT);
+  digitalWrite(motorPin, LOW);
 }
 
 void loop() {
@@ -37,26 +40,34 @@ void loop() {
       messageState = WAITING;
     }
   } else if (messageState == WAITING) {
-    showMessage(index);
-    messageState = SHOWN;
+    if(digitalRead(switchPin) == HIGH){
+      rotateFlag(false);
+
+      delay(500);
+
+      messageState = SHOWN;
+    }
   } else if (messageState == SHOWN) {
+    showMessage(index);
+    digitalWrite(motorPin, HIGH);
+    messageState = READ;
+  } else if (messageState == READ) {
     if(digitalRead(switchPin) == HIGH){
       if(index == messageNum - 1) { index = 0; }
       else { index++; }
 
       delay(500);
 
-      messageState = READ;
+      messageState = ARCAHIVED;
     }
 
     if(index != prevIndex) { showMessage(index); }
 
     prevIndex = index;
-  } else if (messageState == READ) {
+  } else if (messageState == ARCAHIVED) {
+    digitalWrite(motorPin, LOW);
     lcd.clear();
     prevTime = millis();
-
-    rotateFlag(false);
 
     messageState = NORMAL;
   }
@@ -77,8 +88,8 @@ void rotateFlag(bool isUp) {
   if(isUp) { direction = 1; }
   else { direction = -1; }
 
-  for(int i = 1; i <= angle; i++) {
-    myServo.write(i * direction);
+  for(int i = 1; i <= flagAngle; i++) {
+    flagServo.write(i * direction);
     delay(10);
   }
 }
